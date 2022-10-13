3b55e71fa3540e3de49f8b7755ab44be51a1cf47
Afreenkhan2019 <afreen.khan@3pillarglobal.com>
2022-10-13 15:59:07 +0530

    Updated Assignment

---
+++ src/Assignment/ContingCharacters.java
@@ -0,0 +1,27 @@
+package Assignment;
+
+public class ContingCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String b="aabbbdee";
+	    int count=0;
+	    
+		for (int i=0; i<b.length();i++)
+		{			
+			for(int j=0; j<b.length();j++) 
+			{
+				if(b.charAt(i)==b.charAt(j)) 
+				{
+					count++;
+				}
+			}			
+			System.out.print(count+""+b.charAt(i));
+			String d= String.valueOf(b.charAt(i)).trim();
+			b=b.replaceAll(d,"");
+			count =0;	
+			
+		}
+	}
+
+}
--- src/DemoExercise2/JavaExercise.java
+++ src/DemoExercise2/JavaExercise.java
@@ -62,24 +62,7 @@ public class JavaExercise {
 //		    }
 		  
 	
-		String b="aabbbdee";
-		    int count=0;
-		    String k = null;
-			for (int i=0; i<b.length();i++)
-			{			
-				for(int j=0; j<b.length();j++) 
-				{
-					if(b.charAt(i)==b.charAt(j)) 
-					{
-						count++;
-					}
-				}			
-				System.out.print(count+""+b.charAt(i));
-				String d= String.valueOf(b.charAt(i)).trim();
-				b=b.replaceAll(d,"");
-				count =0;	
-				
-			}
+		
 			
 	}
 }
